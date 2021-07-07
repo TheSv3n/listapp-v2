ea5dc5b538c361176b723b592042aa72298b9039
Steven Creighton <ste.creighton@googlemail.com>
2021-07-07 21:03:05 +0100

    07/07/2021

--- frontend/src/components/ListEditor.js
+++ frontend/src/components/ListEditor.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { createList } from "../actions/listActions";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import { CREATE_LIST_RESET } from "../constants/listContstants";
+import { useHistory } from "react-router";
 
 const ListEditor = () => {
   const dispatch = useDispatch();
   const [listName, setListName] = useState("");
+  const history = useHistory();
 
   const handleNewList = (e) => {
     e.preventDefault();
@@ -14,33 +18,47 @@ const ListEditor = () => {
     setListName("");
   };
 
+  const listCreate = useSelector((state) => state.listCreate);
+  const { success, loading, list } = listCreate;
+
+  useEffect(() => {
+    if (success) {
+      dispatch({ type: CREATE_LIST_RESET });
+      history.push(`/list/${list._id}`);
+    }
+  }, [dispatch, history, success, list]);
+
   return (
     <li className="list-group-item my-1 my-md-2 my-lg-2">
-      <form onSubmit={handleNewList}>
-        <div className="row">
-          <div className="input-group col-12  my-1 mr-auto">
-            <div className="input-group-prepend">
-              <div className="input-group-text bg-primary text-white">
-                <i className="fas fa-book" />
+      {loading ? (
+        <Loader />
+      ) : (
+        <form onSubmit={handleNewList}>
+          <div className="row">
+            <div className="input-group col-12  my-1 mr-auto">
+              <div className="input-group-prepend">
+                <div className="input-group-text bg-primary text-white">
+                  <i className="fas fa-book" />
+                </div>
               </div>
-            </div>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="List Name"
-              value={listName}
-              onChange={(e) => setListName(e.target.value)}
-            />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="List Name"
+                value={listName}
+                onChange={(e) => setListName(e.target.value)}
+              />
 
-            <button
-              type="submit"
-              className="btn btn-block btn-primary mx-3 col-5 ml-auto"
-            >
-              Create List
-            </button>
+              <button
+                type="submit"
+                className="btn btn-block btn-primary mx-3 col-5 ml-auto"
+              >
+                Create List
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      )}
     </li>
   );
 };
